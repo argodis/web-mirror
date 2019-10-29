eaea43dac503fe60b3c4827715bec12861030a79
Matthias Greiner <Greiner-Matthias@gmx.de>
2019-10-29 14:33:33 +0100

    first gatsby init

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Welcome to the Argodis website</h1>
+    <p>Here you find all important information.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
